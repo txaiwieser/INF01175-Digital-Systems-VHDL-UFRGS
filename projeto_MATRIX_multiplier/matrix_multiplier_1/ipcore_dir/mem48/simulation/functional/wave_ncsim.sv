b645dd59272885dddb6b9b931e14c0309448d0db
Txai Wieser <txaidw@gmail.com>
2014-07-01 21:21:16 -0300

    asd

---
+++ projeto_MATRIX_multiplier/matrix_multiplier_1/ipcore_dir/mem48/simulation/functional/wave_ncsim.sv
@@ -0,0 +1,22 @@
+
+
+ 
+ 
+ 
+
+
+
+
+window new WaveWindow  -name  "Waves for BMG Example Design"
+waveform  using  "Waves for BMG Example Design"
+
+      waveform add -signals /mem48_tb/status
+      waveform add -signals /mem48_tb/mem48_synth_inst/bmg_port/CLKA
+      waveform add -signals /mem48_tb/mem48_synth_inst/bmg_port/ADDRA
+      waveform add -signals /mem48_tb/mem48_synth_inst/bmg_port/DINA
+      waveform add -signals /mem48_tb/mem48_synth_inst/bmg_port/WEA
+      waveform add -signals /mem48_tb/mem48_synth_inst/bmg_port/CLKB
+      waveform add -signals /mem48_tb/mem48_synth_inst/bmg_port/ADDRB
+      waveform add -signals /mem48_tb/mem48_synth_inst/bmg_port/DOUTB
+
+console submit -using simulator -wait no "run"
